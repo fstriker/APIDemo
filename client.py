663806806529c16dfbd6cb2544e417c4f272a1f3
fstriker <fabi.stricker@gmx.de>
2024-06-12 12:42:40 +0200

    added more dummy endpoints

--- client.py
+++ client.py
@@ -9,6 +9,13 @@ if __name__ == '__main__':
     if status == 200:
         print("Connection successful")
         
-    print(f"Simulating Federated Learning Registration...")
+    print(f"Simulating federated learning process...")
+    print(f"Starting device registration...")
     print(rs.get(f"http://{ip}/register").status_code)
     print(rs.get(f"http://{ip}/register").content)
+    print(f"Fetch selected participants...")
+    print(rs.get(f"http://{ip}/fl-runs/0/participants").content)
+    print(f"Fetch FL configuration...")
+    print(rs.get(f"http://{ip}/fl-runs/0/configuration").content)
+    print(f"Fetch process status...")
+    print(rs.get(f"http://{ip}/fl-runs/0/status").content)
--- server.py
+++ server.py
@@ -13,5 +13,40 @@ def read_root():
 def register_device():
     return {f"Registration was successful, ID: {random.randrange(0,50000)}"}
 
+@app.get("/fl-runs/{")
+def register_device():
+    return {f"Collection of FL runs"}
+
+@app.get("/fl-runs/{runid}/participants")
+def register_device():
+    return {f"The participants with the following IDs are participating: {random.randrange(0,50000)},{random.randrange(0,50000)},{random.randrange(0,50000)},{random.randrange(0,50000)}"}
+
+@app.get("/fl-runs/{runid}/configuration")
+def get_fl_run_configuration():
+    return {f"FL run configuration: {'learning_rate:10', 'fl_rounds:100'}"}
+
+@app.get("/fl-runs/{runid}/global-model")
+def get_fl_run_global_model():
+    return {f"Global model: {[0.32,1.02,123.04,0.230]}"}
+
+@app.get("/fl-runs/{runid}/client-models")
+def post_fl_run_client_models():
+    return {f"POST client model"}
+
+@app.get("/fl-runs/{runid}/evaluation-results")
+def post_fl_run_evaluation_results():
+    return {f"POST evaluation results"}
+
+@app.get("/fl-runs/{runid}/status")
+def get_fl_run_status():
+    return {f"FL run finished"}
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     uvicorn.run(app, port=80, host='0.0.0.0')
